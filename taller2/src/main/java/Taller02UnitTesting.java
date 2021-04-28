ae4c1077a526847869adba9a7e3a62b442311943
Pincheira07 <m.pincheira07@ufromail.cl>
2021-04-28 09:35:38 -0400

    Segundo Commit

--- taller2/src/main/java/Taller02UnitTesting.java
+++ taller2/src/main/java/Taller02UnitTesting.java
@@ -2,9 +2,22 @@ public class Taller02UnitTesting {
     public static void main(String[] args) {
 
     }
+    public static String[] leerArchivo(String ruta){
+        String [] cadenas = new String[20];
+
+
+        return cadenas;
+    }
     public static boolean esPalindromo(String palabra){
-        String invertida = new StringBuilder(palabra).reverse().toString();
-        boolean palindromo = palabra.equals(invertida);
+        String cadena = formatearPalabra(palabra);
+        String invertirPalabra = new StringBuilder(palabra).reverse().toString();
+        boolean palindromo = cadena.equals(invertirPalabra);
         return palindromo;
     }
+    public static String formatearPalabra(String palabra){
+        palabra = palabra.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+                .replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
+        return palabra;
+    }
+
 }
--- taller2/src/test/java/Taller02UnitTestingTest.java
+++ taller2/src/test/java/Taller02UnitTestingTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,7 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class Taller02UnitTestingTest {
 
     @Test
-    void esPalindromo() {
+    void esPalindromoTest() {
+        Taller02UnitTesting pruebaPalindromo = new Taller02UnitTesting();
+        boolean esPalindromo = pruebaPalindromo.esPalindromo("amoraroma");
+        assertTrue(esPalindromo);
+
+
+    }
+    @Test
+    public void esPalindromoFalse(){
+        Taller02UnitTesting pruebaPalindromo = new Taller02UnitTesting();
+        boolean esPalindromo = pruebaPalindromo.esPalindromo("la tele letal");
+        assertFalse(esPalindromo);
+    }
+    @Test
+    public void formatearPalabraTest(){
+        Taller02UnitTesting pruebaPalindromo = new Taller02UnitTesting();
+        String formateada = pruebaPalindromo.formatearPalabra("la tele letal");
+        assertEquals("lateleletal",formateada);
 
     }
 }
